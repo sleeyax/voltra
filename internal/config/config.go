29602daa77334fa6c7c87eb0fdfdf982f3cc7048
Sleeyax <yourd3veloper@gmail.com>
2024-03-01 23:44:30 +0100

    test: verify that the example config is marshalled correctly

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,17 +2,30 @@ package config
 
 import (
 	"github.com/spf13/viper"
+	"path/filepath"
+	"strings"
 )
 
 // Load loads the config file.
 // The config file should be named "config.yaml" and should be in the root of the project.
-func Load() (Configuration, error) {
+// Optionally, you can pass in a list of additional paths to search for the config file.
+func Load(configPaths ...string) (Configuration, error) {
 	var c Configuration
 
 	// Load the config file from disk
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
+	for _, configPath := range configPaths {
+		fileExtension := filepath.Ext(configPath)
+		if fileExtension == ".yml" || fileExtension == ".yaml" {
+			dir, file := filepath.Split(configPath)
+			viper.SetConfigName(strings.Replace(file, fileExtension, "", 1))
+			viper.AddConfigPath(dir)
+		} else {
+			viper.AddConfigPath(configPath)
+		}
+	}
 	if err := viper.ReadInConfig(); err != nil {
 		return c, err
 	}
---
+++ internal/config/config_test.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+	config, err := Load("../../config.example.yml")
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, true, config.EnableTestMode)
+
+	assert.Equal(t, true, config.LoggingOptions.Enable)
+	assert.Equal(t, false, config.LoggingOptions.EnableStructuredLogging)
+
+	assert.Equal(t, "PASTE_YOUR_ACCESS_KEY_HERE", config.Markets.Binance.AccessKey)
+	assert.Equal(t, "PASTE_YOUR_SECRET_KEY_HERE", config.Markets.Binance.SecretKey)
+
+	assert.Equal(t, "USDT", config.TradingOptions.PairWith)
+	assert.Equal(t, float64(15), config.TradingOptions.Quantity)
+	assert.Equal(t, 3, config.TradingOptions.MaxCoins)
+	assert.Equal(t, 2, config.TradingOptions.TimeDifference)
+	assert.Equal(t, 10, config.TradingOptions.RecheckInterval)
+	assert.Equal(t, 30, config.TradingOptions.SellTimeout)
+	assert.Equal(t, float64(10), config.TradingOptions.ChangeInPrice)
+	assert.Equal(t, float64(5), config.TradingOptions.StopLoss)
+	assert.Equal(t, 0.8, config.TradingOptions.TakeProfit)
+	assert.Equal(t, 0.075, config.TradingOptions.TradingFee)
+
+	assert.Equal(t, true, config.TradingOptions.TrailingStopOptions.Enable)
+	assert.Equal(t, 0.4, config.TradingOptions.TrailingStopOptions.TrailingStopLoss)
+	assert.Equal(t, 0.1, config.TradingOptions.TrailingStopOptions.TrailingTakeProfit)
+
+	assert.Equal(t, true, len(config.TradingOptions.AllowList) > 0)
+	assert.Contains(t, config.TradingOptions.AllowList, "AAVE")
+
+	assert.Equal(t, true, len(config.TradingOptions.DenyList) > 0)
+	assert.Contains(t, config.TradingOptions.DenyList, "GBPUSDT")
+}
