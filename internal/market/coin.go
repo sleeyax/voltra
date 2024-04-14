2c37a49333f606dc970f27b7b845974a2061511f
Newtoniano <81423796+Newtoniano@users.noreply.github.com>
2024-04-14 13:30:04 +0200

    fix: empty allow_list pair_with filter

--- internal/market/coin.go
+++ internal/market/coin.go
@@ -45,6 +45,14 @@ func (c Coin) String() string {
 // IsAvailableForTrading checks if the coin should be picked up by the bot for trading.
 // It checks if the coin is in the custom list and if it's not a fiat currency, both of which are options defined in the given config.
 func (c Coin) IsAvailableForTrading(allowList, denyList []string, pairWith string) bool {
+	if len(denyList) > 0 {
+		if utils.Any(denyList, func(fiat string) bool {
+			return c.Symbol == fiat
+		}) {
+			return false
+		}
+	}
+
 	if len(allowList) > 0 {
 		if !utils.Any(allowList, func(allowedCoin string) bool {
 			allowedSymbol := fmt.Sprintf("%s%s", allowedCoin, pairWith)
@@ -52,14 +60,8 @@ func (c Coin) IsAvailableForTrading(allowList, denyList []string, pairWith strin
 		}) {
 			return false
 		}
-	}
-
-	if len(denyList) > 0 {
-		if utils.Any(denyList, func(fiat string) bool {
-			return c.Symbol == fiat
-		}) {
-			return false
-		}
+	} else {
+		return c.Symbol[len(c.Symbol)-len(pairWith):] == pairWith
 	}
 
 	return true
